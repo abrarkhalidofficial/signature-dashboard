8dd4b4216056bd641a316af2dc6e0044490430a8
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-11-03 04:53:41 -0700

    edit info structure

--- src/Screens/BuildingInformation.js
+++ src/Screens/BuildingInformation.js
@@ -2,5 +2,10 @@ import React from "react";
 import "../Styles/buildingInformation.scss";
 
 export default function BuildingInformation() {
-  return <div>BuildingInformation</div>;
+  return (
+    <div className="panel__container">
+      <div className="panel__container__header">Floor 4</div>
+      <div className="panel__container__table"></div>
+    </div>
+  );
 }
--- src/Screens/EditBuildingInformation.js
+++ src/Screens/EditBuildingInformation.js
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function EditBuildingInformation() {
-  return <div>EditBuildingInformation</div>;
+  return (
+    <div className="panel__container">
+      <div className="panel__container__header">Floor 4</div>
+      <form className="panel__container__form">
+        <div className="panel__container__form__row">
+          <div className="panel__container__form__input">
+            <div className="panel__container__form__input__label">
+              Floor Name
+            </div>
+            <input
+              type="text"
+              placeholder="Gym Floor"
+              className="panel__container__form__input__field"
+            />
+          </div>
+          <div className="panel__container__form__input">
+            <div className="panel__container__form__input__label">
+              Floor Name
+            </div>
+            <input
+              type="text"
+              placeholder="Gym Floor"
+              className="panel__container__form__input__field"
+            />
+          </div>
+          <button className="panel__container__form__button">
+            Save Changes
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
